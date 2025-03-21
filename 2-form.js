1d61252a5c6477764b24e1d58163eec4434cb4f1
Lynohodd <hennadii.lytvyn@gmail.com>
2025-03-21 21:54:09 +0000

    Deploying to gh-pages from @ Lynohodd/goit-js-hw-09@1cd05dc810a1c6c3819af9fc6de1b813d65e1766 🚀

---
+++ 2-form.js
@@ -0,0 +1,17 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const e=basicLightbox.create(`<form class="feedback-form" autocomplete="off">
+        <label class="label email-label">
+          Email
+          <input
+            class="text-in email-input"
+            type="email"
+            name="email"
+            autofocus
+          />
+        </label>
+        <label class="label message-label">
+          Message
+          <textarea class="text-in textarea" name="message" rows="8"></textarea>
+        </label>
+        <button class="button" type="submit">Submit</button>
+      </form>`);e.show();
+//# sourceMappingURL=2-form.js.map
